77bbaaa7afcb8d4c5ccb4af88e768052de4d7af2
myk07 <renospoyl@yahoo.gr>
2017-01-04 00:48:58 +0200

    Controller Changes
    Changes on controlleer for the first pages for application.

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,18 @@ namespace App\Http\Controllers;
 		}
 
 		public function getAbout(){
-			return view('pages.about');
+			$first = 'Eirineos';
+			$last = 'Poularinos';
+			$email = 'renospoyl@yahoo.gr';	
+			// concatenate the first and last name 
+			$fullname = $first . " " . $last;
+			
+			$data = [];
+			$data['email'] = $email;
+			$data['fullname'] = $fullname;
+			
+										// Call special method "with" to view the fullname.
+			return view('pages.about')->withData($data);
 		}
 
 		public function getContact(){
