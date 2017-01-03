e75579901d4e733a2d300e192d995f9dfe5ceb5b
myk07 <renospoyl@yahoo.gr>
2017-01-04 01:08:45 +0200

    Create Welcome page for app.
    The code is clearly using @section.

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,7 +3,7 @@
  
   
   @section('content')
-        <div class="row">
+         <div class="row">
             <div class="col-md-12">
                 <div class="jumbotron">
                   <h1>Welcome to Social Calendar Application</h1>
