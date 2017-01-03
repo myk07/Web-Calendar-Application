4e36b0562afb744121d6118113771cb729dc4154
myk07 <renospoyl@yahoo.gr>
2017-01-04 01:04:01 +0200

    Create a pages for application.
    Create the welcome page, the main page, the about page and the contact
    page with blade.php.
    Create a partials folder to contain the content of app pages.

---
+++ resources/views/pages/welcome.blade.php
@@ -0,0 +1,34 @@
+
+@extends('main')
+ 
+  
+  @section('content')
+        <div class="row">
+            <div class="col-md-12">
+                <div class="jumbotron">
+                  <h1>Welcome to Social Calendar Application</h1>
+                  <p class="lead">Thank you so much for visiting. 
+                   Log In or SignUp.</p>
+                  <p><a class="btn btn-primary btn-lg" href="#" role="button">Login</a></p>
+                  <p><a class="btn btn-primary btn-lg" href="#" role="button">SignUp</a></p>
+                </div>
+            </div>
+        </div> <!-- end of header .row -->
+
+        <div class="row">
+            <div class="col-md-8">
+                <div class="post">
+                    <h3>Smart scheduling for meetings</h3>
+                    <p>Schedule events quickly by checking coworkers’ availability or layering their calendars in a single view. You can share calendars so people see full event details or just if you are free. </p>
+                    <a href="#" class="btn btn-primary">Read More</a>
+                </div>
+                <hr>
+               
+            </div>
+            <div class="col-md-3 col-md-offset-1">
+                <h2>Sidebar</h2>
+            </div>
+
+  @endsection
+
+  
