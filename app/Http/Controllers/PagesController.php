6d2df601f953082a738d62e5a735273ede861a06
myk07 <renospoyl@yahoo.gr>
2017-01-13 20:54:49 +0200

    Change calendar page controller and create construct for middleware

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,29 +3,19 @@
 namespace App\Http\Controllers;
 
 	class PagesController extends Controller {
+		public function __construct() {
+			$this->middleware('auth');
+		}
 
 		public function getIndex(){
 			return view('pages.welcome');
 		}
 
-		public function getAbout(){
-			$first = 'Eirineos';
-			$last = 'Poularinos';
-			$email = 'renospoyl@yahoo.gr';	
-			// concatenate the first and last name 
-			$fullname = $first . " " . $last;
-			
-			$data = [];
-			$data['email'] = $email;
-			$data['fullname'] = $fullname;
-			
-										// Call special method "with" to view the fullname.
-			return view('pages.about')->withData($data);
+		 public function getCalendar(){
+			return view('pages.calendar');
 		}
 
-		public function getContact(){
-			return view('pages.contact');
-		}
+		
 
 
 	}
