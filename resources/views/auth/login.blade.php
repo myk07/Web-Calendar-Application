087a7bd370a2ffa378d71e2d38c2643b0c3bbeb5
myk07 <renospoyl@yahoo.gr>
2017-01-13 21:02:41 +0200

    Change login page
    Vhange login page for authentication

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading" align='center'>Login</div>
+                <div class="panel-heading">Login</div>
                 <div class="panel-body">
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
                         {{ csrf_field() }}
@@ -26,6 +26,7 @@
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password" class="col-md-4 control-label">Password</label>
+
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password">
 
@@ -36,9 +37,6 @@
                                 @endif
                             </div>
                         </div>
-
-                       
-                        
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
@@ -52,7 +50,9 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                               <p><a class="btn btn-primary btn-lg" href="http://poularinos.tripherald.com/" role="button">Login</a></p>
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Login
+                                </button>
 
                                 <a class="btn btn-link" href="{{ url('/password/reset') }}">Forgot Your Password?</a>
                             </div>
